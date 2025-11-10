c763b9056036a9d3db4d0722a38d65a1589db58b
chamnguyenhuyen-ui <chamnguyenhuyen@gmail.com>
2025-11-10 15:14:19 +0700

    lab3

--- Lab/lab3.js
+++ Lab/lab3.js
@@ -1,1 +1,17 @@
 // bai 1 Chuyển các hàm sau thành arrow functions:
+// Function 1
+const multiply = (a, b) => a * b;
+
+// Function 2
+const isPositive = number => number >= 0;
+
+// Function 3
+const getRandomNumber = () => Math.random();
+
+// Function 4
+document.addEventListener("click", () => {
+  console.log("Clicked!");
+});
+// bai 2 
+// Bài tập 2: Sử dụng Default Parameters
+// Viết lại hàm sau sử dụng default parameters:
